b0e8769cd6fa69837157554173bc475a62461bf4
LandonTopy <landontopy@gmail.com>
2023-09-15 09:32:16 -0400

    l

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("What is the number?");
+        number = scan.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd");
+        }
     }
 }
